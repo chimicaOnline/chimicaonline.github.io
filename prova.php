234c946868e070b75f6266fd81d77c7fc3c0d37e
chimicaOnline <79723404+chimicaOnline@users.noreply.github.com>
2021-02-27 10:50:43 +0100

    Add files via upload

---
+++ prova.php
@@ -0,0 +1,12 @@
+<?php
+?>
+
+<html lang="en-US">
+  <title>Prova pagina PHP</title>
+  <link rel="shortcut icon" href="logo.png?" type="image/png" />
+  
+  <center>
+  <h2>Prova pagina PHP</h2>
+  <br><br><hr><br><br>
+  <a>Prova di pagina PHP su github pages</a>
+</html>
